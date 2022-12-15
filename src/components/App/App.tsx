4c19fe7c21f5cfdaf4f2e5a0b60e75f8c1572e0e
Aditya Nath <aditya.k.nath@gmail.com>
2022-12-15 20:00:50 +1030

    Add scss to project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import logo from "assets/logo.svg";
-import "./App.css";
+import "./App.scss";
 
 const App = () => {
     const title = "Learn React";
@@ -7,11 +7,7 @@ const App = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit
-                    <code>src/App.tsx</code>
-                    and save to reload.
-                </p>
+                <p>This is home page</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
